a7c89493bea598c34253c196a467002099e3e7ef
NiyatiMudgal <98032662+NiyatiMudgal@users.noreply.github.com>
2023-08-16 19:37:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload() {   
+}
+function setup() {
+    canvas = createCanvas(450,450);
+    canvas.center();
+}
+function draw() {
+    circle(30, 30, 20);
+    rect(30, 20, 55, 55);
+    ellipse(56, 46, 55, 55);
+}
